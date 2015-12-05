8201480af42a73fd132505eba6755aee8a795594
vtphan <vphan@memphis.edu>
2015-12-05 11:19:32 -0600

    config.go is meant for users to set indexType

--- config.go
+++ config.go
@@ -1,8 +1,10 @@
 package fmic
 
 // indexType is the type of the indices of the sequence to be indexed.
-// If indexType is uint32, the length of sequence must be smaller than 2^32
-// For really long sequences, set indexType to int64
+// If indexType is uint32, the length of sequence must be smaller than 2^32.
+// For example, uint32 can be used for the human genome.  But int64 must
+// be used a collective metagenome that is longer than 4Gbp.
+
 type indexType int64
 
 // NUM_BYTES is the number of bytes of indexType
