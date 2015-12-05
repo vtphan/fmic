500c63f136d6870be75829f45a3f8afd8a191e54
vtphan <vphan@memphis.edu>
2015-12-05 11:11:43 -0600

    config.go is meant for users to set indexType

--- config.go
+++ config.go
@@ -1,8 +1,9 @@
+package fmic
 
-// indexType is the type of the indices of the BWT
-// if indexType is uint32, the length of sequence must be smaller than 2^32
-// for really long sequences, set indexType to int64
+// indexType is the type of the indices of the sequence to be indexed.
+// If indexType is uint32, the length of sequence must be smaller than 2^32
+// For really long sequences, set indexType to int64
 type indexType int64
 
 // NUM_BYTES is the number of bytes of indexType
-var NUM_BYTES = uint(8)
+const NUM_BYTES = uint(8)
