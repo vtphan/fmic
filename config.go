2e5fd47a4b2e0506dab9591d0d3301c4c026be85
vtphan <vphan@memphis.edu>
2016-02-06 15:14:14 -0600

    automatic setting of sizes of indexType and sequenceType

--- config.go
+++ config.go
@@ -1,5 +1,9 @@
 package fmic
 
+import (
+	"unsafe"
+)
+
 // indexType is the type of the indices of the sequence to be indexed.
 // If indexType is uint32, the length of sequence must be smaller than 2^32.
 // For example, uint32 can be used for the human genome.  But int64 must
@@ -8,11 +12,12 @@ package fmic
 type indexType int64
 
 // The number of bytes of indexType
-const indexTypeBytes = uint(8)
+const indexTypeBytes = uint(unsafe.Sizeof(indexType(0)))
+
 
 // The number of sequences to be indexed together must be storable by a regionType
 // For example, use uint16 if there are no more than 2^16 sequences to be indexed at once.
 type sequenceType uint16
 
 // The number of bytes of regionType
-const sequenceTypeBytes = 2
+const sequenceTypeBytes = uint(unsafe.Sizeof(sequenceType(0)))
